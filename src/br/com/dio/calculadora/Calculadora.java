b3c62e705f3bd35b7eaf0e0b277c5c6f7f166013
DaianeTararam <daalkmim@gmail.com>
2023-03-01 16:33:13 -0300

    Criando class livro, porém código ainda contendo erros.

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -36,7 +36,8 @@ public class Calculadora {
 
     public static int multiplicar(int a, int b) {
         return a * b;
-    }public static int dividir(int a, int b) {
+    }
+    public static int dividir(int a, int b) {
         return a / b;
     }
 
